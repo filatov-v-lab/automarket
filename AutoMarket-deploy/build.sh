6d77ddaadcf73923c0ff4715dcf9397d56e488e2
filatov-v-lab <morkovichyt@gmail.com>
2026-06-04 01:06:59 +0300

    ez update 1.2

--- AutoMarket-deploy/build.sh
+++ AutoMarket-deploy/build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-# Скрипт сборки для Render.com
 set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py collectstatic --no-input --clear
