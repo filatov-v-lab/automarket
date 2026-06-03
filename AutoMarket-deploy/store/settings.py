82143782bc0040f35e07202fd65998d436d00e05
filatov-v-lab <morkovichyt@gmail.com>
2026-06-04 01:24:17 +0300

    Update settings.py

--- AutoMarket-deploy/store/settings.py
+++ AutoMarket-deploy/store/settings.py
@@ -84,7 +84,6 @@ USE_TZ = True
 # Статические файлы — WhiteNoise раздаёт их напрямую без Nginx
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main', 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
